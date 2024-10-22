5831fe6d88ed3f79593e830903bc39ee0b4a99a8
mukesh-m23 <mukeshmedikonda9@gmail.com>
2024-10-22 15:11:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
   plugins: [react()],
   base:"/"
 })
